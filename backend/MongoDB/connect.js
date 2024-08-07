5166ec6ea58aafd650f4589d98ea691c056ccd07
Vikrant-Mainwal <vikrantmainwal1301@gmail.com>
2024-08-07 21:39:15 +0530

    mongo url

--- backend/MongoDB/connect.js
+++ backend/MongoDB/connect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDb = ()=>{
     mongoose.set("strictQuery",true);
-    mongoose.connect("mongodb+srv://20227057:DJTGSD@cluster0.arels2a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>console.log("MongoDb Connected")).catch((error)=>console.error(error))
+    mongoose.connect(process.env.MONGODB_URL).then(()=>console.log("MongoDb Connected")).catch((error)=>console.error(error))
 }
 
 export default connectDb
